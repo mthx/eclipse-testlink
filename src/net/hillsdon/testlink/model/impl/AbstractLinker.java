97d0919052529c4aaf742ee4ee54163c4bd7385b
Matt Hillsdon <matt@hillsdon.net>
2012-01-31 16:58:08 +0000

    Add JUnit4 support (finally!).

--- src/net/hillsdon/testlink/model/impl/AbstractLinker.java
+++ src/net/hillsdon/testlink/model/impl/AbstractLinker.java
@@ -10,17 +10,19 @@
  *******************************************************************************/
 package net.hillsdon.testlink.model.impl;
 
+import static java.util.Arrays.asList;
+
 import java.util.Collection;
 
 import net.hillsdon.testlink.model.ILinker;
 import net.hillsdon.testlink.model.Linkage;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.IAnnotation;
+import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.ITypeHierarchy;
 import org.eclipse.jdt.core.JavaModelException;
-
-import static java.util.Arrays.asList;
 
 /**
  * Provides an implementation of {@link #getLinkageFor(IType)} based on super-class
@@ -33,12 +35,29 @@ public abstract class AbstractLinker implements ILinker {
   private static final Collection<String> TEST_SUPER_CLASSES = asList("junit.framework.TestCase");
 
   public Linkage getLinkageFor(final IType type) throws JavaModelException {
-    ITypeHierarchy supertypeHierarchy = type.newSupertypeHierarchy(new NullProgressMonitor());
+    final ITypeHierarchy supertypeHierarchy = type.newSupertypeHierarchy(new NullProgressMonitor());
+    // JUnit 4.
+    for (IMethod method : type.getMethods()) {
+      for (IAnnotation annotation : method.getAnnotations()) {
+        final String name = annotation.getElementName();
+        if (name != null) {
+          final String[][] resolutions = type.resolveType(name);
+          if (resolutions != null) {
+            for (String[] resolved : resolutions) {
+              if ("org.junit".equals(resolved[0])) {
+                return Linkage.TEST;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    // JUnit 3.
     for (IType superClass : supertypeHierarchy.getAllSuperclasses(type)) {
       if (TEST_SUPER_CLASSES.contains(superClass.getFullyQualifiedName())) {
         return Linkage.TEST;
       }
-      // Will need to inspect annotations for JUnit4.
     }
     return Linkage.IMPL;
   }
